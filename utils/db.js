baa452b716c65fbe3ef72637e867c72cf88acaef
mar1fatih <marouanefatih631@gmail.com>
2024-09-28 15:53:59 +0100

    task1

--- utils/db.js
+++ utils/db.js
@@ -1,37 +1,37 @@
 import { MongoClient } from 'mongodb';
 
 class DBClient {
-    host = process.env.DB_HOST;
-    port = process.env.DB_PORT;
-    database = process.env.DB_DATABASE;
-    constructor() {
-      if (!this.host) {
-        this.host = '127.0.0.1';
-      }
-      if (!this.port) {
-        this.port = 27017;
-      }
-      if (!this.database) {
-        this.database = 'files_manager';
-      }
-      MongoClient(`mongodb://${this.host}:${this.port}`, (err, client) => {
-        this.db = client.db(this.database);
-        this.users = this.db.collection('users');
-        this.files = this.db.collection('files');
-      });
+  constructor() {
+    let host = process.env.DB_HOST;
+    let port = process.env.DB_PORT;
+    let database = process.env.DB_DATABASE;
+    if (!host) {
+      host = '127.0.0.1';
     }
+    if (!port) {
+      port = 27017;
+    }
+    if (!database) {
+      database = 'files_manager';
+    }
+    MongoClient(`mongodb://${host}:${port}`, (err, client) => {
+      this.db = client.db(database);
+      this.users = this.db.collection('users');
+      this.files = this.db.collection('files');
+    });
+  }
 
-    isAlive() {
-      return Boolean(this.db);
-    }
+  isAlive() {
+    return Boolean(this.db);
+  }
 
-    async nbUsers() {
-      return this.users.countDocuments();
-    }
+  async nbUsers() {
+    return this.users.countDocuments();
+  }
 
-    async nbFiles () {
-      return this.files.countDocuments();
-    }
+  async nbFiles() {
+    return this.files.countDocuments();
+  }
 }
 
 const dbClient = new DBClient();
