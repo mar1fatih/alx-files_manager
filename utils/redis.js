c0977303f1d5c2f04e86ab7c1974c68ae94c4b2c
mar1fatih <marouanefatih631@gmail.com>
2024-09-28 14:53:03 +0100

    task0

---
+++ utils/redis.js
@@ -0,0 +1,30 @@
+import redis from 'redis';
+import { promisify } from 'util';
+
+class RedisClient {
+  constructor() {
+    this.client = redis.createClient();
+    this.client.on('error', (err) => console.log(err));
+    this.AsyncGet = promisify(this.client.get).bind(this.client);
+  }
+
+  isAlive() {
+    return this.client.connected;
+  }
+
+  async get(key) {
+    const res = await this.AsyncGet(key);
+    return res;
+  }
+
+  async set(key, value, exp) {
+    this.client.setex(key, exp, value);
+  }
+
+  async del(key) {
+    this.client.del(key);
+  }
+}
+
+const redisClient = new RedisClient();
+module.exports = redisClient;
