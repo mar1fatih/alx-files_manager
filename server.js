5d0ca756bcfe5a9c6a62f0f2539c09573d1f3782
mar1fatih <marouanefatih631@gmail.com>
2024-09-30 15:30:48 +0100

    task3

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 import express from 'express';
+import bodyParser from 'body-parser';
 import router from './routes/index';
 
 const app = express();
 const port = process.env.PORT || 5000;
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use('/', router);
 
 app.listen(port);
+
+export default app;
