0b0d1c57de5feb4eff649b626b21966b3d984bc8
mar1fatih <marouanefatih631@gmail.com>
2024-09-30 14:19:05 +0100

    task3

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,30 +1,24 @@
 import crypto from 'crypto';
 import dbClient from '../utils/db';
 
-const postNew = async (req) => {
+const postNew = async (req, res) => {
   const { email, password } = req.body;
   if (!email) {
-    return {
-      error: 'Missing email',
-    };
+    return res.status(400).json({error: 'Missing email'});
   }
   if (!password) {
-    return {
-      error: 'Missing password',
-    };
+    return res.status(400).json({error: 'Missing password'});
   }
   const user = await dbClient.users.findOne({ email });
   if (user) {
-    return {
-      error: 'Already exist',
-    };
+    return res.status(400).json({error: 'Already exist'});
   }
   const hached = crypto.createHash('sha1').update(password).digest('hex');
   const newUser = await dbClient.users.insertOne({ email, password: hached });
-  return {
+  return res.status(201).json({
     id: newUser.insertedId,
     email,
-  };
+  });
 };
 
 module.exports = { postNew };
--- routes/index.js
+++ routes/index.js
@@ -12,13 +12,6 @@ router.get('/stats', async (req, res) => {
   res.status(200).send(await AppController.getStats());
 });
 
-router.post('/users', async (req, res) => {
-  const user = await UsersController.postNew(req);
-  if ('error' in user) {
-    res.status(400).send(user);
-  } else {
-    res.status(201).send(user);
-  }
-});
+router.post('/users', UsersController.postNew);
 
 module.exports = router;
