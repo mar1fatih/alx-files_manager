88eedac6ca2d0cc4d4904c7ba3df2c6dea6bfb6f
mar1fatih <marouanefatih631@gmail.com>
2024-10-02 14:15:55 +0100

    task 4

--- server.js
+++ server.js
@@ -1,12 +1,9 @@
 import express from 'express';
-import bodyParser from 'body-parser';
 import router from './routes/index';
 
 const app = express();
-const port = parseInt(process.env.PORT, 10) || 5000;
+const port = process.env.PORT || 5000;
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use('/', router);
